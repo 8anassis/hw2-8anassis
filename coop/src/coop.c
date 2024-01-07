b4d15b2ad18ccbffc820f17027864a0e5bfce281
8anassis <146738580+8anassis@users.noreply.github.com>
2024-01-07 15:32:50 +0200

    Update coop.c

--- coop/src/coop.c
+++ coop/src/coop.c
@@ -1,1 +1,28 @@
+#include <stdio.h>
 
+int main(void){
+    int choice, count=0;    // the player's choice of move and a counter for rounds.
+    putchar('C');           // first move always cooperate.
+    putchar('\n');
+    while (1) {
+        choice = getchar(); // to store players move.
+        if (choice != EOF){ 
+            
+            if ((count == 9) && (choice=='d' || choice=='D' || choice=='c' || choice=='C')){    //in every 9 rounds always play 'Cooperate'.
+                putchar('C');
+                putchar('\n');
+                count = 0;     // reset counter
+            }
+            else if (choice=='d' || choice=='D' || choice=='c' || choice=='C'){
+                putchar(choice);    // next move is players' previous move.
+                putchar('\n');
+                count++;
+            }
+            else
+            continue; // if not the right character ignore and continue.
+        }
+        else
+        break;  // if you reach EOF, break the loop.
+    }
+    return 0;
+}
