8f48992176d05459ff3731441dece8551a82eaa3
8anassis <146738580+8anassis@users.noreply.github.com>
2024-01-08 18:23:18 +0200

    Update coop.c
    correcting the while loop statment

--- coop/src/coop.c
+++ coop/src/coop.c
@@ -1,31 +1,30 @@
 #include <stdio.h>
 
 int main(void){
-    int choice, count=0;    // the player's choice of move and a counter for rounds.
-    putchar('C');           // first move always cooperate.
+    int choice, count=0;                                    // the player's move and a counter for rounds.
+    putchar('C');                                           // first move always cooperate.
     putchar('\n');
-    while (1) {
-        choice = getchar(); // to store players move.
-        if (choice != EOF){             
-            if ((count == 9) && (choice=='D' || choice=='C')){    //in every 9 rounds always play 'Cooperate'.
+    
+    while ((choice = getchar()) != EOF) {
+                     
+        if ((count == 9) && (choice=='D' || choice=='C')){  // every 10 rounds always play 'Cooperate'.
                 putchar('C');
                 fflush(stdin);
                 putchar('\n');
                 fflush(stdin);
-                count = 0;     // reset counter
-            }
-            else if (choice=='D' || choice=='C'){
-                putchar(choice);    // next move is players' previous move.
+                count = 0;                                 // reset counter
+        }
+        else if (choice=='D' || choice=='C'){
+                putchar(choice);                           // next move is players' previous move.
                 fflush(stdin);
                 putchar('\n');
                 fflush(stdin);
                 count++;
-            }
-            else{
-                fflush(stdin);
-                continue; // if not the right character ignore and continue.
-            }                
         }
-        break;  // if you reach EOF, break the loop.
-    }return 0;
+        else{
+                fflush(stdin);
+                continue;                                 // if not the right character ignore and continue.
+        }                
+    }
+    return 0;
 }
