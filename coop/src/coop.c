9a76f2a163838e7099edcd8fbff18da71f4e7174
8anassis <146738580+8anassis@users.noreply.github.com>
2024-01-09 16:09:56 +0200

    Update coop.c
    adding counter for maximum rounds.
    correcting fflush in stdout

--- coop/src/coop.c
+++ coop/src/coop.c
@@ -1,29 +1,31 @@
 #include <stdio.h>
 
 int main(void){
-    int choice, count=0;                                    // the player's move and a counter for rounds.
-    putchar('C');                                           // first move always cooperate.
+    int choice, count=0, rounds=0;                                    // the player's move, a counter (count) for every 10 rounds so that can play cooparate and a counter (rounds) for maximum rounds.
+    putchar('C');                                                     // first move always cooperate.
     putchar('\n');
     
-    while ((choice = getchar()) != EOF) {
+    while (((choice = getchar()) != EOF) && (rounds<=1E6)) {
                      
-        if ((count == 9) && (choice=='D' || choice=='C')){  // every 10 rounds always play 'Cooperate'.
+        if ((count == 9) && (choice=='D' || choice=='C')){             // every 10 rounds always play 'Cooperate'.
                 putchar('C');
-                fflush(stdin);
+                fflush(stdout);
                 putchar('\n');
-                fflush(stdin);
-                count = 0;                                 // reset counter
+                fflush(stdout);
+                count = 0;                                             // reset counter
+                rounds++;                                              //increase total rounds
         }
         else if (choice=='D' || choice=='C'){
-                putchar(choice);                           // next move is players' previous move.
-                fflush(stdin);
+                putchar(choice);                                       // next move is players' previous move.
+                fflush(stdout);
                 putchar('\n');
-                fflush(stdin);
+                fflush(stdout);
                 count++;
+                rounds++;
         }
         else{
                 fflush(stdin);
-                continue;                                 // if not the right character ignore and continue.
+                continue;                                             // if not the right character ignore and continue.
         }                
     }
     return 0;
