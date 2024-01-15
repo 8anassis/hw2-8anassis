06d134d9e174c6107292f02b2cd0bf39a97f1432
8anassis <146738580+8anassis@users.noreply.github.com>
2024-01-15 15:31:50 +0200

    some comments and removing some fflush

--- coop/src/coop.c
+++ coop/src/coop.c
@@ -1,46 +1,43 @@
 #include <stdio.h>
 
+/* Start playing as Cooparate. Every 10 times of opponents' Defective move, play 'C' 
+and every 50 times of opponents' Cooperate move, play 'D' */
+
 int main(void){
     int choice, cntD=0, cntC=0, rounds=0;                                  
     putchar('C');
-    fflush(stdout);                                                   
     putchar('\n');
     fflush(stdout);
         
     do{
         choice=getchar();
-        fflush(stdin);              
-        
-        if      (choice=='C' && cntC!=100){
+               
+        if      (choice=='C' && cntC!=50){
                 putchar('C');
-                fflush(stdout);
                 putchar('\n');
                 fflush(stdout);                        
                 cntC++;
                 rounds++;
         }
 
-        if      (choice=='C' && cntC==100){
+        if      (choice=='C' && cntC==50){
                 putchar('D');
-                fflush(stdout);
                 putchar('\n');
                 fflush(stdout);
                 cntC=0;
                 rounds++;
         }
 
-        if      (choice=='D' && cntD==9){
+        if      (choice=='D' && cntD==10){
                 putchar('C');
-                fflush(stdout);                                      
                 putchar('\n');
                 fflush(stdout);
                 cntD=0;
                 rounds++;
         }
 
-        if      (choice=='D' && cntD!=9){
+        if      (choice=='D' && cntD!=10){
                 putchar('D');
-                fflush(stdout);
                 putchar('\n');
                 fflush(stdout);                        
                 cntD++;
